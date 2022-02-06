5bc6c7c4db483e6242e244d5fc7a123dea283dfc
Mario Visic <mario@mariovisic.com>
2022-02-06 11:38:38 +1030

    Fix rendering of grid and tiled layers to look correct
    Using the grid size seems to result in the layers rendering correctly,
    I'm not using the width and height anymore, so unsure what they're used
    for, except for defining how many tiles across the layer is.

--- lib/ruby2d/tiled/level.rb
+++ lib/ruby2d/tiled/level.rb
@@ -15,24 +15,26 @@ module Ruby2d
       def show
         Window.set(background: @data['__bgColor'])
 
-        (@data['layerInstances'].select do |layer|
+        (@data['layerInstances'].reverse.select do |layer|
           ['Tiles', 'IntGrid', 'AutoLayer'].include?(layer['__type'])
         end).each do |layer|
           if layer['gridTiles'].size > 0 || layer['autoLayerTiles'].size > 0
             tileset = Ruby2D::Tileset.new(
               File.expand_path(File.join(@tileset_relative_path, layer['__tilesetRelPath'])),
-              tile_width: layer['__cWid'],
-              tile_height: layer['__cHei'],
+              tile_width: layer['__gridSize'],
+              tile_height: layer['__gridSize'],
               padding: 0, # FIXME: implement padding
               spacing: 0, # FIXME: implement spacing
             )
 
             grid_tiles = layer['gridTiles'] + layer['autoLayerTiles']
 
-            # FIXME: This needs tweaking, it does not render correctly
+
+            # FIXME: We use the gridSize and not the width and height, i'm not sure if that's OK
+            # Will need to fix this later
             grid_tiles.each do |tile|
               tile_id = SecureRandom.uuid
-              tileset.define_tile(tile_id, tile['src'][0], tile['src'][1])
+              tileset.define_tile(tile_id, tile['src'][0] / layer['__gridSize'], tile['src'][1] / layer['__gridSize'])
               tileset.set_tile(tile_id, [{ x: tile['px'][0], y: tile['px'][1] }])
             end
           elsif layer['intGridCsv'].size > 0
